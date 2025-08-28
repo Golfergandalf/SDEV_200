005c381b1ec0b79fcae6da246359cf079fff89bd
Gabby Willard <gabbywillard@icloud.com>
2025-08-28 12:55:46 -0400

    update

--- M01/M01_Assignment_1.java
+++ M01/M01_Assignment_1.java
@@ -5,6 +5,20 @@ Date:   8/25/25
 File:   M01_Assignment_1.java
 */
 
+/* 6.9 (Conversions between feet & meters)
+Write a class that contains the following two methods:
+    • Convert from feet to meters
+    public static double footToMeter(double foot)
+    • Convert from meters to feet
+    public static double meterToFoot(double meter)
+
+The formula for the conversion is:
+    • meter = 0.305 * foot
+    • foot = 3.279 * meter
+
+Write a test program that invokes these methods to display the following tables.
+*/
+
 public class M01_Assignment_1 {
     public static void main(String args[]) {
         System.out.println("Hello World!");
