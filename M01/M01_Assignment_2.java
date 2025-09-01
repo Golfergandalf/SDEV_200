c000e5b8adb3fd8151f3ae8c541d188e3e94c068
Gabby Willard <gabbywillard@icloud.com>
2025-08-31 22:16:46 -0400

    update

--- M01/M01_Assignment_2.java
+++ M01/M01_Assignment_2.java
@@ -58,41 +58,51 @@ public class M01_Assignment_2 {
     }
     
     // Methods
-    
+/* Return true if the card number is valid */
+    public static boolean isValid (long number) {
+        return false;
+    }
+
+/* Get the result from Step 2 */
+    public static int sumOfDoubleEvenPlace (long number) {
+        return 0;
+    }
+
+    /* Return this number if it is a single digit, otherwise, return the sum of the two digits */
+    public static int getDigit (int number) {
+        if (number < 10) {
+            // if number is under 10, return number
+            return number;
+        } else {
+            // if number is greater than 10, return the sum of the two digits
+            return (number / 10) + (number % 10);
+        }
+    }
+
+/* Return sum of odd-place digits in number */
+    public static int sumOfOddPlace (long number) {
+        return 0;
+    }
+
+    /* Return true if the number d is a prefix for number */
+    public static boolean prefixMatched (long number, int d) {
+        return getPrefix(number, getSize(d)) == d;
+    }
+
     /* Return the number of digits in d */
     public static int getSize (long d) {
         return String.valueOf(d).length();
     }
-    /* Return this number if it is a single digit, otherwise, return the sum of the two digits */
-    public static int getDigit (int number) {
-        if (number < 10) {
-            // if number is under 10, then return number
-            return number;
-        } else {
-            // if number is greater than 10, then return the sum of the two digits
-            return (number / 10) + (number % 10);
-        }
-    }
-    
+
     /* Return the first k number of digits from number. If the number of digits in number is less than k, return number. */
     public static long getPrefix (long number, int k) {
-        return 0;
-    }
-    /* Return true if the number d is a prefix for number */
-    public static boolean prefixMatched (long number, int d) {
-        return false;
-    }
-
-    /* Return true if the card number is valid */
-    public static boolean isValid (long number) {
-        return false;
-    }
-    /* Get the result from Step 2 */
-    public static int sumOfDoubleEvenPlace (long number) {
-        return 0;
-    }
-    /* Return sum of odd-place digits in number */
-    public static int sumOfOddPlace (long number) {
-        return 0;
+        String numString = String.valueOf(number);
+        if (numString.length() <= k) {
+            // When less than k, return number
+            return number;
+        } else {
+            // When greater than k
+            return Long.parseLong(numString.substring(0, k));
+        }
     }
 }
