bbe434e42a8740d9f662bb2f8ec57a97946e309b
Gabby Willard <gabbywillard@icloud.com>
2025-09-01 02:47:05 -0400

    update

--- M02/M02_Assignment_1.java
+++ M02/M02_Assignment_1.java
@@ -4,3 +4,8 @@ Class:  SDEV 200
 Date:   9/3/25
 File:   M02_Assignment_1.java
 */
+public class M02_Assignment_1 {
+    public static void main(String args[]) {
+        // !
+    }
+}
--- M02/M02_Assignment_2.java
+++ M02/M02_Assignment_2.java
@@ -4,3 +4,8 @@ Class:  SDEV 200
 Date:   9/3/25
 File:   M02_Assignment_2.java
 */
+public class M02_Assignment_2 {
+    public static void main(String args[]) {
+        // !
+    }
+}
