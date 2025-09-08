4fc9ac6841c2751eca2baa0998bef1c1ccad2e3e
Gabby Willard <gabbywillard@icloud.com>
2025-09-08 11:09:44 -0400

    update

--- M03/M03_Assignment_2.java
+++ M03/M03_Assignment_2.java
@@ -5,6 +5,26 @@ Date:   9/8/25
 File:   M03_Assignment_2.java
 */
 
+/* 13.15 (Use BigInteger for the Rational class) 
+Redesign and implement the Rational class in LiveExample 13.13 using BigInteger for the numerator and denominator.
+Write a test program that prompts the user to enter two rational numbers and display the results as shown in the following sample run:
+Sample Run for Exercise13_15.java
+---
+Enter the input for the program (Sample data provided below. You may modify it.)
+343 531
+4912 9347
+---
+Execution Result:
+---
+Enter rational r1 with numerator and denominator separated by a space: 343 531
+Enter rational r2 with numerator and denominator separated by a space: 4912 9347
+343/531 + 4912/9347 = 5814293/4963257
+343/531 - 4912/9347 = 597749/4963257
+343/531 * 4912/9347 = 1684816/4963257
+343/531 / 4912/9347 = 3206021/2608272
+4912/9347 is 0.5255162084091152
+*/
+
 public class M03_Assignment_2 {
     public static void main(String[] args) {
         // !
