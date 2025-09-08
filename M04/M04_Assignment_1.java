ea844f9e02f3eb06835b2b1d1f62d69f6bb0de95
Gabby Willard <gabbywillard@icloud.com>
2025-09-08 12:47:52 -0400

    update

--- M04/M04_Assignment_1.java
+++ M04/M04_Assignment_1.java
@@ -13,6 +13,23 @@ A Java program contains various pairs of grouping symbols, such as:
 Note the grouping symbols cannot overlap. For example, (a{b)} is illegal.
 Write a program to check whether a Java source-code file has correct pairs of grouping symbols.
 Pass the source-code file name as a command-line argument.
+
+Sample Run for Exercise20_11.java
+---
+Enter command arguements (Sample arguments provided below. You may modify it)
+---
+c:\book\Welcome.java
+---
+Execution Result:
+---
+JDK8>java Exercise20_11 c:\book\Welcome.java
+public class Welcome {
+	public static void main(String[] args) {
+		// Display message Welcome to Java! on the console
+		System.out.println("Welcome to Java!");
+	}
+}
+Correct grouping pairs
 */
 
 public class M04_Assignment_1 {
