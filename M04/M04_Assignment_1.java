68607a101ecfa9f5afc03c7a8a68f53b5be8d1c7
Gabby Willard <gabbywillard@icloud.com>
2025-09-08 12:42:43 -0400

    update

--- M04/M04_Assignment_1.java
+++ M04/M04_Assignment_1.java
@@ -5,6 +5,17 @@ Date:   9/8/25
 File:   M04_Assignment_1.java
 */
 
+/* 20.11 (Match grouping symbols)
+A Java program contains various pairs of grouping symbols, such as:
+    • Parentheses: ( and )
+    • Braces: { and }
+    • Brackets: [ and ]
+
+Note the grouping symbols cannot overlap. For example, (a{b)} is illegal.
+Write a program to check whether a Java source-code file has correct pairs of grouping symbols.
+Pass the source-code file name as a command-line argument.
+*/
+
 public class M04_Assignment_1 {
     public static void main(String[] args) {
         // Assignment 1
