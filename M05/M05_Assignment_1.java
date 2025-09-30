d10b7240abe2281a9982e908692e2b4d15f7ecfa
Gabby Willard <gabbywillard@icloud.com>
2025-09-30 12:40:17 -0400

    update

--- M05/M05_Assignment_1.java
+++ M05/M05_Assignment_1.java
@@ -5,6 +5,14 @@ Date:   9/22/25
 File:   M05_Assignment_1.java
 */
 
+/* 14.1 (Display images)
+Write a program that displays four images in a grid pane, as shown in Figure 14.43a.
+Figure 14.43: 
+(a) Exercise 14.1 displays four images. 
+(b) Exercise 14.2 displays a tic-tac-toe board with images. 
+(c) Three cards are randomly selected.
+*/
+
 public class M05_Assignment_1 {
     public static void main(String[] args) {
         // Assignment 1
