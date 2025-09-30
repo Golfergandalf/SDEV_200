e18ae10ec0f17aaa0984642ab33895789c3d21a5
Gabby Willard <gabbywillard@icloud.com>
2025-09-30 12:46:12 -0400

    update

--- M05/M05_Assignment_2.java
+++ M05/M05_Assignment_2.java
@@ -5,6 +5,10 @@ Date:   9/22/25
 File:   M05_Assignment_2.java
 */
 
+/* 15.7 (Change color using a mouse)
+Write a program that displays the color of a circle as black when the mouse button is pressed, and as white when the mouse button is released.
+*/
+
 public class M05_Assignment_2 {
     public static void main(String[] args) {
         // Assignment 2
--- M05/M05_Assignment_3.java
+++ M05/M05_Assignment_3.java
@@ -5,6 +5,16 @@ Date:   9/22/25
 File:   M05_Assignment_3.java
 */
 
+/* 16.3 (Traffic lights)
+Write a program that simulates a traffic light. The program lets the user select one of three lights: red, yellow, or green. 
+When a radio button is selected, the light is turned on. Only one light can be on at a time (see Figure 16.37a). 
+No light is on when the program starts.
+Figure 16.37: 
+(a) The radio buttons are grouped to let you turn only one light on at a time. 
+(b) The program converts miles to kilometers and vice versa. 
+(c) The program converts among decimal, hex, and binary numbers.
+*/
+
 public class M05_Assignment_3 {
     public static void main(String[] args) {
         // Assignment 3
